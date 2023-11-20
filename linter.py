6d72ad0a4542c11c9ed01c3e294d65d5b3fc5286
Pat Gaffney <pat@hypepat.com>
2023-11-20 15:25:19 -0600

    feat: support multi-line compile time errors

--- linter.py
+++ linter.py
@@ -1,5 +1,6 @@
 import json
 import logging
+import re
 
 from SublimeLinter.lint import Linter, LintMatch
 
@@ -20,11 +21,35 @@ class StaticCheck(Linter):
 
     # A dict of defaults for the linter’s settings.
     defaults = {
-        'selector': 'source.go'
+        'selector': 'source.go',
     }
 
     # Turn off stdin. The staticheck command requires a file.
     template_suffix = '-'
+
+    # Regex for handling compile errors.
+    compile_regex = re.compile("^(?P<fileName>.*\.go)\:(?P<lineNumber>\d+)\:(?P<columnNumber>\d+)\:\s+(?P<message>.*)")
+
+    def handle_compile_error(self, message):
+        """
+        Special helper to handle compile errors. These are combined
+        into a single error in staticcheck so we expand them here.
+        """
+        m = self.compile_regex.match(message)
+        if m is not None:
+            fileName = self.context.get('folder') + '/' + m.group('fileName')
+            lineNumber = int(m.group('lineNumber'))
+            columnNumber = int(m.group('columnNumber'))
+            message = m.group('message')
+            return LintMatch(
+                code='compile',
+                filename=fileName,
+                line=lineNumber - self.line_col_base[0],
+                col=columnNumber - self.line_col_base[0],
+                error_type='error',
+                message=message,
+            )
+
 
     def find_errors(self, output):
         """
@@ -46,28 +71,35 @@ class StaticCheck(Linter):
                 logger.warning(e)
                 self.notify_failure()
 
-            code = data['code']
-            error_type = data['severity']
-            filename = data['location']['file']
-            line = data['location']['line'] - self.line_col_base[0]
-            col = data['location']['column'] - self.line_col_base[0]
-            message = data['message']
 
-            # Clean up the dependency message.
-            if message.startswith("could not analyze dependency"):
-                message = data['message'].split('[')[0]
+            if data['code'] == 'compile':
+                for msg in data['message'].split('\n'):
+                    m = self.handle_compile_error(msg)
+                    if m is not None:
+                        yield m
+            else:
+                code = data['code']
+                error_type = data['severity']
+                filename = data['location']['file']
+                line = data['location']['line'] - self.line_col_base[0]
+                col = data['location']['column'] - self.line_col_base[0]
+                message = data['message']
 
-            # Ensure we don't set negative line/col combinations.
-            if line < 0:
-                line = 0
-            if col < 0:
-                col = 0
+                # Clean up the dependency message.
+                if message.startswith("could not analyze dependency"):
+                    message = data['message'].split('[')[0]
 
-            yield LintMatch(
-                code=code,
-                filename=filename,
-                line=line,
-                col=col,
-                error_type=error_type,
-                message=message,
-            )
+                # Ensure we don't set negative line/col combinations.
+                if line < 0:
+                    line = 0
+                if col < 0:
+                    col = 0
+
+                yield LintMatch(
+                    code=code,
+                    filename=filename,
+                    line=line,
+                    col=col,
+                    error_type=error_type,
+                    message=message,
+                )
